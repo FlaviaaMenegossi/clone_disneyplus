2b443684f50ea7d7d7de302a5c54215a26ca923f
FlaviaaMenegossi <flavinha.mene12@outlook.com>
2025-11-12 15:22:33 -0300

    criando a seção de planos

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ const gulp = require ('gulp');
 const sass = require ('gulp-sass')(require('sass'));
 const cleanCss = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+
+
 
 
 function styles(){
@@ -18,11 +22,21 @@ function images(){
         .pipe(gulp.dest('./dist/images'));
 }
 
+// Copiar e minificar JS
+function scripts() {
+  return gulp.src('./src/scripts/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/scripts'));
+}
 
 
 
 
-exports.default = gulp.parallel(styles,images);
+
+exports.default = gulp.parallel(styles,images, scripts);
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/**/*.js', gulp.parallel(scripts));
 }
